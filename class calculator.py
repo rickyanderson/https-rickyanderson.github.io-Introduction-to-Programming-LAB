d2a2e189c2c39ba0111d8d977ef19361e347afe8
Ricky Anderson Ricco <43334333+rickyanderson@users.noreply.github.com>
2018-10-16 14:45:53 +0700

    Add files via upload

--- class calculator.py
+++ class calculator.py
@@ -1,7 +1,8 @@
 class cal():
-    def __init__(self,satu,dua):
+    def __init__(self,satu,dua): #to innitiate the needed variable
         self.a=satu
         self.b=dua
+    #to set the calculating proccess in the class
     def add(self):
         return self.a+self.b
     def mul(self):
@@ -10,10 +11,13 @@ class cal():
         return self.a/self.b
     def sub(self):
         return self.a-self.b
+#to input number that going to be proccess
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
-choice=(input("enter choice:"))
-obj=cal(a,b)
+print("Choose choice to make:\n","1) add\n","2) substract\n","3) multiply\n","4) divide\n")
+choice=(input("enter choice:"))#to input choice of what to do with the number
+obj=cal(a,b) #to set the class as obj
+#to determine which choices to make
 if choice == "1":
     print(obj.add())
 elif choice == "2":
